1227f374d5e7ff819b2d28c9eb0df6029158f529
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2024-10-01 17:21:37 -0300

    PUSH E POP

--- api-aula-1/script.js
+++ api-aula-1/script.js
@@ -1,15 +1,54 @@
 const express = require('express')
-
 const app = express();
 
-const port = 3000
+app.use(express.json())
 
 
-app.get('/', (req, res) =>{
-    res.send('Hello World!')
+let personList = []
+
+const port = 3000;
+
+const nome = "T-55A";
+
+
+app.get('/visualizar', (req, res) =>{
+    res.send(personList)
 })
+
+app.post(`/params/:id`, (req, res) =>{
+
+    const {id} = req.params;
+    res.send(id)
+})
+
+
+app.delete(`/deletar`, (req, res) =>{
+
+    const {name, age} = req.body
+    personList.pop()
+    res.send(`O usuário foi removido! o nome do usuário foi ${name}`)
+})
+
+
+app.put(`/update`, (req, res) =>{
+    
+    const{name, age} = req.body
+    personList.put("CPF")
+    res.send(`Os dados do usuário foi alterado! esse usuário foi ${name}`)
+})
+
+
+ app.post('/cadastrar', (req,res) => {
+    const { name, age} = req.body;
+    personList.push({name, age})
+     res.send(`Usuário recebido! nome do usuários ${name}`)
+
+ })
 
 app.listen(port, () =>{
     console.log(`Example app listening on port ${port}`)
 })
+
+
+console.log("onça pintada")
 
